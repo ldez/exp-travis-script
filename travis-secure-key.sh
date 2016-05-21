7697682c28852d90bcd93103dcc78206d28772eb
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2016-05-21 18:42:54 +0200

    chore: better secure key script

--- travis-secure-key.sh
+++ travis-secure-key.sh
@@ -2,33 +2,42 @@
 set -e
 
 ## Here, you will need to replace <org@email>, <somewhere> and <token>
-USER_EMAIL="<org@email>"
-PROJECT_DIRECTORY="<somewhere>"
-SSH_KEY_NAME="travis_rsa"
-GITHUB_TOKEN="<token>"
+USER_EMAIL='<org@email>'
+PROJECT_DIRECTORY='<somewhere>'
+SSH_KEY_NAME='travis_rsa'
+GITHUB_TOKEN='<token>'
 
-## First you create a RSA public/private key pair just for Travis.
-ssh-keygen -t rsa -C "${USER_EMAIL}" -f ~/.ssh/${SSH_KEY_NAME}
+generateKey() {
+  ## First you create a RSA public/private key pair just for Travis.
+  ssh-keygen -t rsa -C "${USER_EMAIL}" -f ~/.ssh/${SSH_KEY_NAME}
 
-## Copy to clipboard.
-##
-## Then following the official doc (https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/),
-xclip -sel clip < ~/.ssh/${SSH_KEY_NAME}.pub
+  ## Copy to clipboard.
+  ## https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account
+  xclip -sel clip < ~/.ssh/${SSH_KEY_NAME}.pub
 
-echo 'Paste your key into the "Key" field ; Click "Add key" ; Confirm the action by entering your GitHub password'
+  echo 'Paste your key into the "Key" field ; Click "Add key" ; Confirm the action by entering your GitHub password'
+}
 
 commandExists() {
   command -v "$1" 1>/dev/null 2>&1 && return 0 || return 1
 }
 
-## Ruby dev dependencies via RVM, needed by Travis CLI
-# rvm install 2.3.0-dev
+installTravis() {
+  ## Ruby dev dependencies via RVM (https://rvm.io), needed by Travis CLI
+  # rvm install 2.3.0-dev
+
+  ## Install Travis CLI if necessary.
+  commandExists travis || sudo gem install travis
+
+  ## Logged in to Travis CI
+  # travis login --github-token ${GITHUB_TOKEN}
+}
+
+## Generate SSH key
+generateKey
 
 ## Install Travis CLI if necessary.
-commandExists travis || sudo gem install travis
-
-## Logged in to Travis CI
-# travis login --github-token ${GITHUB_TOKEN}
+installTravis
 
 ## Let's go to where your ".travis.yml" is
 cd ${PROJECT_DIRECTORY}
